784ee68cf40bdaf0e73e7e0d1afe0eb70240b3c7
Valen <mvalentinacalogeropulos@gmail.com>
2025-12-03 21:49:40 -0300

    fix: change extension of logo .png to .svg

--- src/components/layout/AdminSidebar.tsx
+++ src/components/layout/AdminSidebar.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/logo.png"; // Asegúrate de guardar tu imagen aquí
+import logo from "../../assets/logo.svg";
 
 interface Props {
   activeView: string;
