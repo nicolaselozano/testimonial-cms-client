b62d5e2f74d5f24cdb31e98f934ccbcb1000050b
nicolaselozano <nicoeloza12@gmail.com>
2025-12-09 20:21:14 -0300

    fix: imports

--- src/components/login/AuthButtons.tsx
+++ src/components/login/AuthButtons.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 axios.defaults.withCredentials = true; // importante para enviar cookies
